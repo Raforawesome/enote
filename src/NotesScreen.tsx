63f7c0c041f9576ac07767df7a58130601abc735
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-06-14 19:13:08 -0400

    Add sidebar CSS styling

--- src/NotesScreen.tsx
+++ src/NotesScreen.tsx
@@ -1,16 +1,23 @@
 import type { Component } from 'solid-js';
 import { invoke } from '@tauri-apps/api';
 
-import './css/styles.global.css'
+// import './css/styles.global.css'
 import './css/NotesScreen.module.css';
 
 
 const App: Component = () => {
 	return (
 	  <div>
-		<h1 class="center">Notes</h1>
+		<button style={
+			"background-color: transparent;border:none;color:#FFFFFF;font-size:large;margin:1vh 0 0 1vh;font-weight:700;"
+		}>{"<"} Back</button>
+
+		<h1>Notes</h1>
 		<hr/>
 		<textarea />
+		<div class="sidebar">
+			<p>test</p>
+		</div>
 	  </div>
 	);
 };
